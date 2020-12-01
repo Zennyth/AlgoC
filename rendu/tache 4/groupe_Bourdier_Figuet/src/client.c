cbcee114b4f122090f7dee145c3c3c7674710456
Zennyth <mathis.figuet@orange.fr>
2020-12-01 11:41:58 +0100

    Merge tache 4

---
+++ rendu/tache 4/groupe_Bourdier_Figuet/src/client.c
@@ -0,0 +1,183 @@
+/*
+ * SPDX-FileCopyrightText: 2020 John Samuel
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>  
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "client.h"
+#include "bmp.h"
+#include "json.h"
+
+void analyse(char *pathname, char *data) {
+  //compte de couleurs
+  couleur_compteur *cc = analyse_bmp_image(pathname);
+
+  int count;
+  //strcpy(data, "{code:\"couleurs\",valeurs:[");
+  char nbCouleurs[100];
+  printf("Votre nombre de couleurs (max 30): ");
+  fgets(nbCouleurs, 1024, stdin);
+  int n = atoi(nbCouleurs);
+  char temp_string[15] = "30,";
+  if (n != 30) {
+    sprintf(temp_string, "%i,", n);
+  }
+  strcat(data, temp_string);
+  
+  //choisir 30 couleurs
+  for (count = 1; count < (n+1) && cc->size - count >0; count++) {
+    if(cc->compte_bit ==  BITS32) {
+      sprintf(temp_string, "\"#%02x%02x%02x\",", cc->cc.cc24[cc->size-count].c.rouge,cc->cc.cc32[cc->size-count].c.vert,cc->cc.cc32[cc->size-count].c.bleu);
+    }
+    if(cc->compte_bit ==  BITS24) {
+      sprintf(temp_string, "\"#%02x%02x%02x\",", cc->cc.cc32[cc->size-count].c.rouge,cc->cc.cc32[cc->size-count].c.vert,cc->cc.cc32[cc->size-count].c.bleu);
+    }
+    strcat(data, temp_string);
+  }
+  data[strlen(data)-1] = '\0';
+  //enlever le dernier virgule 
+}
+
+/* 
+ * Fonction d'envoi et de réception de messages
+ * Il faut un argument : l'identifiant de la socket
+ */
+
+int envoie_recois_message(char *pathname) {
+  
+  char data[1024];
+  // la réinitialisation de l'ensemble des données
+  memset(data, 0, sizeof(data));
+
+  // Exemple d'entré pour couleurs
+  //char typeMessage[100] = "\"couleurs\"";
+  char typeMessage[100] = "message";
+  /*
+  printf("Votre type de message (max 1000 caracteres): ");
+  fgets(typeMessage, 1024, stdin);
+  strtok(typeMessage, "\n");*/
+  char message[500] = "";
+
+  if(strcmp(typeMessage, "couleurs") == 0 || strcmp(typeMessage, "balises") == 0 || strcmp(typeMessage, "plot") == 0) {
+    analyse(pathname, message);
+    sprintf(data, "{\"code\":\"%s\",\"valeurs\":[%s]}", typeMessage, message);
+  } else {
+    // Demandez à l'utilisateur d'entrer un message
+    // Exemple d'entré pour couleurs
+    //char message[100] = "10,\"#123456\",\"#123456\",\"#123456\",\"#123456\",\"#123456\"";
+    printf("Votre message %s (max 1000 caracteres): ", message);
+    fgets(message, 1024, stdin);
+    strtok(message, "\n");
+    sprintf(data, "{\"code\":\"%s\",\"valeurs\":[\"%s\"]}", typeMessage, message);
+  }
+
+  struct Json res = parse(strdup(data));
+
+  if (strcmp(res.code, "\"error\"") == 0) {
+    print_array(res);
+    perror("erreur convertion json");
+    return -1;
+  } else {
+    // Si l'entré utilisateur est bien convertissable en json alors
+    int socketfd;
+    int bind_status;
+
+    struct sockaddr_in server_addr, client_addr;
+
+    /*
+    * Creation d'une socket
+    */
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if ( socketfd < 0 ) {
+      perror("socket");
+      exit(EXIT_FAILURE);
+    }
+
+    //détails du serveur (adresse et port)
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    //demande de connection au serveur
+    int connect_status = connect(socketfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if ( connect_status < 0 ) {
+      perror("connection serveur");
+      exit(EXIT_FAILURE);
+    }
+    int write_status = write(socketfd, data, strlen(data));
+    if ( write_status < 0 ) {
+      perror("erreur ecriture");
+      exit(EXIT_FAILURE);
+    }
+
+    // la réinitialisation de l'ensemble des données
+    memset(data, 0, sizeof(data));
+
+    // lire les données de la socket
+    int read_status = read(socketfd, data, sizeof(data));
+    if ( read_status < 0 ) {
+      perror("erreur lecture ");
+      return -1;
+    }
+
+    printf("%s\n", data);
+  }
+ 
+  return 0;
+}
+
+int envoie_couleurs(int socketfd, char *pathname) {
+  char data[1024];
+  memset(data, 0, sizeof(data));
+  analyse(pathname, data);
+  int write_status = write(socketfd, data, strlen(data));
+  if ( write_status < 0 ) {
+    perror("erreur ecriture");
+    exit(EXIT_FAILURE);
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  int socketfd;
+  int bind_status;
+
+  struct sockaddr_in server_addr, client_addr;
+
+  /*
+   * Creation d'une socket
+   */
+  /*
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  if ( socketfd < 0 ) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  //détails du serveur (adresse et port)
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+
+  //demande de connection au serveur
+  int connect_status = connect(socketfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+  if ( connect_status < 0 ) {
+    perror("connection serveur");
+    exit(EXIT_FAILURE);
+  }
+  */
+  envoie_recois_message(argv[1]);
+  //envoie_couleurs(socketfd, argv[1]);
+  close(socketfd);
+}
---
+++ rendu/tache 4/groupe_Bourdier_Figuet/src/json.c
@@ -0,0 +1,133 @@
+/*
+ * SPDX-FileCopyrightText: 2020 John Samuel
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * 
+ * Json librairy minify
+ *
+ */
+
+#define _GNU_SOURCE //to avoid c99 related warnings
+#include "json.h"
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>  
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// Display the json
+void print_array(struct Json j1)
+{
+  printf("{\n");
+  printf("\t\"code\": %s,\n", j1.code);
+  printf("\t\"valeurs\": [\n");
+	for (int i = 0; i < NUMBER_OF_STRING; i++)
+	{
+    if(j1.valeurs[i][0] != '\0') {
+      printf("\t\t%s,\n", j1.valeurs[i]);
+    }
+	}
+  printf("\t]\n");
+  printf("}\n");
+}
+
+// Convert json to str
+char * toString(struct Json j1) {
+  static char raw[255];
+  sprintf(raw, "{\"code\":%s,\"valeurs\":[", j1.code);
+  for (int i = 0; i < NUMBER_OF_STRING; i++)
+	{
+    if(j1.valeurs[i][0] != '\0') {
+      strcat(raw, j1.valeurs[i]);
+    }
+	}
+  strcat(raw, "]}");
+  return raw;
+}
+
+// Validate a string in json format
+bool validator(char element[]) {
+  bool res = true;
+  if(element[0] == '"' && element[strlen(element) - 1] == '"') {
+    //Valide pour une string
+    //printf("str => %s\n", element);
+    if(element[1] == '"' || element[strlen(element) - 2] == '"') {
+      res = false;
+    }
+  } else {
+    int num = atoi(element);
+    if(num == 0 && element[0] != '0') {
+      res = false;
+      //String sans ""
+      //printf("error => %s\n", element);
+    } else {
+      //Nombre
+      //printf("number => %s\n", element);
+      res = true;
+    }
+  }
+  return res;
+}
+
+// Check and convert string into a json object
+struct Json parse(char src[])
+{
+  struct Json json = { "", { "" }};
+  char delim[] = ":,{[]}";
+  char *ptr = strtok(src, delim);
+  int i = 1;
+  
+  while(ptr != NULL) {
+    if(validator(ptr) == true){
+      // Pour chaque valeur valide dans le string (src)
+      if(i == 1) { // indice pour la propriété code
+        char *temp = strdup(ptr);
+        if(strcmp(ptr, "\"code\"") != 0) {
+          char *code = strdup(ptr);
+          strcpy(json.code, "\"error\"");
+          strcpy(json.valeurs[0], "\"Vous n'avez pas défini la propiété code !\"");
+          break;
+        }
+      } else if(i == 2) { // indice pour le code
+        if(strcmp(ptr, "\"nom\"") == 0 || strcmp(ptr, "\"message\"") == 0 || strcmp(ptr, "\"couleurs\"") == 0 || strcmp(ptr, "\"balises\"") == 0 || strcmp(ptr, "\"calcul\"") == 0 || strcmp(ptr, "\"plot\"") == 0 ) {
+          char *code = strdup(ptr);
+          strcpy(json.code, code);
+        } else {
+          char *code = "\"error\"";
+          strcpy(json.valeurs[0], "\"Vous valeur de code est incorrect !\"");
+          strcpy(json.valeurs[1], "\"Valeurs disponible :\"");
+          strcpy(json.valeurs[2], "\"nom\"");
+          strcpy(json.valeurs[3], "\"message\"");
+          strcpy(json.valeurs[4], "\"couleurs\"");
+          strcpy(json.valeurs[5], "\"balises\"");
+          strcpy(json.valeurs[6], "\"calcul\"");
+          strcpy(json.valeurs[7], "\"plot\"");
+          strcpy(json.code, code);
+          break;
+        }
+      } else if(i == 3) { // indice pour la propriété code
+        char *temp = strdup(ptr);
+        if(strcmp(ptr, "\"valeurs\"") != 0) {
+          char *code = strdup(ptr);
+          strcpy(json.code, "\"error\"");
+          strcpy(json.valeurs[0], "\"Vous n'avez pas défini la propiété valeurs !\"");
+          break;
+        }
+      }
+      else if(i >= 4) { // indice pour les valeurs
+        char *temp = strdup(ptr);
+        strcpy(json.valeurs[i-4], temp);
+      }
+    } else {
+      char *code = "\"error\"";
+      strcpy(json.code, code);
+      strcpy(json.valeurs[0], "\"Définition du json incorrect !\"");
+    }
+    ptr = strtok(NULL, delim);
+    i++;
+  }
+  return json;
+}
